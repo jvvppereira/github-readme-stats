755f91275ad433a99aef38b31cf8def7332e4619
João Vítor <joao.vitor.paes@hotmail.com>
2026-09-01 15:16:57 +0200

    Change Card component height from 99% to 100%

--- src/common/Card.js
+++ src/common/Card.js
@@ -245,7 +245,7 @@ class Card {
           x="0.5"
           y="0.5"
           rx="${this.border_radius}"
-          height="99%"
+          height="100%"
           stroke="${this.colors.borderColor}"
           width="${this.width - 1}"
           fill="${
